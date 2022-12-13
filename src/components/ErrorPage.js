bf4a25178ab8c51cc990e61dc32af1e3d7859b04
Thomas Fouquert <tfouquert@gmail.com>
2022-12-13 08:27:15 +0100

    update error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,29 +1,42 @@
-import React from 'react'
-import butterfly from "../images/butterfly.png"
+import React from "react";
+import butterfly from "../images/butterfly.png";
+import { Link } from "react-router-dom";
 
 const centerH2 = {
-    textAlign: 'center',
-    marginTop: '50px',
-    fontFamily: 'Fasthand, recursive',
-    fontSize: '36px'
-}
-
-
+  textAlign: "center",
+  marginTop: "50px",
+  fontFamily: "Fasthand, recursive",
+  fontSize: "36px",
+};
 
 const centerImg = {
-    display: "block",
-    margin: "40px auto"
-}
+  display: "block",
+  margin: "40px auto",
+};
 
 const ErrorPage = () => {
-    return (
-        <div className="index-item">
-            <div className="container">
-                <h2 style={centerH2}>Oups, wrong direction!</h2>
-                <img style={centerImg} src={butterfly} alt="error page" className='img-fluid' />
-            </div>
+  return (
+    <div className="index-item">
+      <div className="container">
+        <h2 style={centerH2}>Oups, wrong direction!</h2>
+        <div className="text-center">
+          <Link
+            to="/"
+            className="btn btn-primary mt-2 "
+            style={{ backgroundColor: "hotpink", border: "1px solid hotpink" }}
+          >
+            Go Back
+          </Link>
         </div>
-    )
-}
+        <img
+          style={centerImg}
+          src={butterfly}
+          alt="error page"
+          className="img-fluid"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
--- src/components/MyCookie.js
+++ src/components/MyCookie.js
@@ -1,29 +1,29 @@
-import React from 'react'
+import React from "react";
 import CookieConsent from "react-cookie-consent";
 
 const MyCookie = () => {
-
-
   return (
     <CookieConsent
       enableDeclineButton
       flipButtons
       cookieName={`castr_cookie`}
-      style={{ background: "rgb(90, 40, 4)" }}
-      buttonText={'accepter'}
-      declineButtonText={'refuser'}
+      style={{ background: "#b4ac9c" }}
+      buttonText={"accepter"}
+      declineButtonText={"refuser"}
       buttonStyle={{ background: "white", color: "black", fontSize: "13px" }}
-      declineButtonStyle={{ background: "black" ,color: "white", fontSize: "13px" }}
+      declineButtonStyle={{
+        background: "black",
+        color: "white",
+        fontSize: "13px",
+      }}
       cookieValue={true}
       declineCookieValue={false}
       expires={150}
-      >
-      Nous utilisons nos cookies pour personnaliser le contenu et pour analyser notre trafic.
+    >
+      Nous utilisons nos cookies pour personnaliser le contenu et pour analyser
+      notre trafic.
+    </CookieConsent>
+  );
+};
 
-      </CookieConsent>
-
-
-  )
-}
-
-export default MyCookie
+export default MyCookie;
