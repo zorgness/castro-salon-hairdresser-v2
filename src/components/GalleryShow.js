42a299fad3e9691a3416ce66a2b8f47e8dc06908
Thomas Fouquert <tfouquert@gmail.com>
2022-12-14 01:10:07 +0100

    update on gallery show

--- src/components/GalleryShow.js
+++ src/components/GalleryShow.js
@@ -4,6 +4,7 @@ import { capitalizeFirstLetter } from "../util/capitalize";
 import { useFetchGallery } from ".././customHooks/useFetchData";
 import GalleryImageComponent from "./GalleryImageComponent";
 import Loader from "../components/Loader";
+import { Link } from "react-router-dom";
 
 const GalleryShow = () => {
   const params = useParams();
@@ -18,31 +19,47 @@ const GalleryShow = () => {
 
   if (status === "done") {
     return (
-      <div className="content-container index-item1">
-        <div className="bubble3" style={{ backgroundColor: "lavender" }}></div>
+      <>
+        <div style={{ background: "white", padding: "40px" }}></div>
+        <div className="content-container index-item1">
+          <div
+            className="bubble3"
+            style={{ backgroundColor: "lavender" }}
+          ></div>
 
-        <h1
-          className="pattaya text-center m-5 text-decoration-underline"
-          style={{ fontSize: "48px", position: "relative" }}
-        >
-          {capitalizeFirstLetter(data?.title ?? "")}
-        </h1>
+          <h1
+            className="pattaya text-center m-5 text-decoration-underline"
+            style={{ fontSize: "48px", position: "relative" }}
+          >
+            {capitalizeFirstLetter(data?.title ?? "")}
+          </h1>
 
-        <div className="d-flex justify-content-center m-4">
+          <div className="d-flex justify-content-center m-4">
+            <div>
+              <p
+                style={{ width: "380px", fontSize: "20px" }}
+                className="text-start popins"
+              >
+                {data?.text}
+              </p>
+            </div>
+          </div>
+
           <div>
-            <p
-              style={{ width: "380px", fontSize: "20px" }}
-              className="text-start popins"
+            <GalleryImageComponent galleryId={data?.id} />
+          </div>
+
+          <div className="text-center m-5">
+            <Link
+              to="/gallerie"
+              className="btn btn-dark"
+              style={{ borderRadius: "40px" }}
             >
-              {data?.text}
-            </p>
+              retour
+            </Link>
           </div>
         </div>
-
-        <div>
-          <GalleryImageComponent galleryId={data?.id} />
-        </div>
-      </div>
+      </>
     );
   } else {
     return null;
